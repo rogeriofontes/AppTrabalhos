062f45561d47a0e86094de176d522b3beae7d53a
Rogério Fontes <fontestz@gmail.com>
2020-05-18 19:54:29 -0300

    Menu com internacinalizacao

--- app/src/main/java/br/com/unipac/apptrabalhos/MainActivity.java
+++ app/src/main/java/br/com/unipac/apptrabalhos/MainActivity.java
@@ -31,17 +31,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listaDeAlunos = (ListView) findViewById(R.id.lista_aluno);
-        formBtn = (Button) findViewById(R.id.formBtn);
+        //formBtn = (Button) findViewById(R.id.formBtn);
 
         alunoRepository = new AlunoRepository(this);
 
-        formBtn.setOnClickListener(new View.OnClickListener() {
+        /*/formBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), AlunoForm.class);
                 startActivity(i);
             }
-        });
+        }); */
     }
 
     @Override
@@ -60,25 +60,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+    public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        switch (id){
-            case R.id.item1:
-                Intent i = new Intent(getApplicationContext(), AlunoForm.class);
+        int idItem = item.getItemId();
+
+        switch (idItem) {
+            case  R.id.menuAdd:
+                Intent i = new Intent(this, AlunoForm.class);
                 startActivity(i);
                 return true;
-            case R.id.item2:
-                Toast.makeText(getApplicationContext(),"Item 2 Selected",Toast.LENGTH_LONG).show();
-                return true;
-            case R.id.item3:
-                Toast.makeText(getApplicationContext(),"Item 3 Selected",Toast.LENGTH_LONG).show();
+            case R.id.menuList:
+                Toast.makeText(this, "Não tenho lista", Toast.LENGTH_SHORT).show();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
--- app/src/main/java/br/com/unipac/apptrabalhos/ui/AlunoForm.java
+++ app/src/main/java/br/com/unipac/apptrabalhos/ui/AlunoForm.java
@@ -2,12 +2,16 @@ package br.com.unipac.apptrabalhos.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import br.com.unipac.apptrabalhos.MainActivity;
 import br.com.unipac.apptrabalhos.R;
 import br.com.unipac.apptrabalhos.model.domain.Aluno;
 import br.com.unipac.apptrabalhos.model.repository.AlunoRepository;
@@ -58,5 +62,32 @@ public class AlunoForm extends AppCompatActivity {
     public static class ViewHolder {
         EditText nameEdt, registerEdt;
         Button saveBtn;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_form, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int idItem = item.getItemId();
+
+        switch (idItem) {
+            case  R.id.menuAdd:
+                //this.finish();
+                startActivity(getIntent());
+                return true;
+            case R.id.menuList:
+                Intent i = new Intent(this, MainActivity.class);
+                startActivity(i);
+                return true;
+            case R.id.menuEdit:
+                Toast.makeText(this, "Ainda não temos Edição", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
